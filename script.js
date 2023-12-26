a4d88b83d5a4d1be6bcbd1a6c54595bc6d4c7dce
KajalPourjalil <rina.skyler@yahoo.com>
2023-12-26 14:04:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,5 +10,22 @@ function isInc(arr) {
 const a1 = [3, 5, 5, 10, 34];
 const a2 = [5, 12, 13, 20];
 
-console.log(isInc(a1));
-console.log(isInc(a2));
+// console.log(isInc(a1));
+// console.log(isInc(a2));
+
+//............................
+
+const arr1 = [2, 4, 5, 6, 19];
+const arr2 = [1, 3, 7, 10, 20];
+const arr3 = [2, 4, 5, 6, 19];
+
+function compare(arr1, arr2) {
+    for (let i = 0; i < arr1.length; i++) {
+        if (!arr2.includes(arr1[i])) {
+            return false; //if not equal
+        }
+    }
+    return true; //if equal
+}
+
+console.log(compare(arr1, arr3));
