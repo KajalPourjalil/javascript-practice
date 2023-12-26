c2cfd3ea820240df434e99689a78236a79f7e7ae
KajalPourjalil <rina.skyler@yahoo.com>
2023-12-26 14:25:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,17 +15,44 @@ const a2 = [5, 12, 13, 20];
 
 //............................
 
-const arr1 = [2, 4, 5, 6, 19];
-const arr2 = [1, 3, 7, 10, 20];
-const arr3 = [2, 4, 5, 6, 19];
+const a4 = [2, 4, 5, 6, 19, 21, 22];
+const a5 = [1, 3, 7, 10, 20];
+const a3 = [2, 4, 5, 6, 19];
+const a6 = [2, 4, 5, 6, 19];
 
 function compare(arr1, arr2) {
     for (let i = 0; i < arr1.length; i++) {
-        if (!arr2.includes(arr1[i])) {
-            return false; //if not equal
+        if (arr2.includes(arr1[i])) {
+            return true; //if equal
         }
     }
-    return true; //if equal
+    return false; //if not equal
 }
 
-console.log(compare(arr1, arr3));
+// console.log(compare(arr1, arr3));
+
+//.............................................
+
+function isEqual(arr1, arr2) {
+    return arr1.length === arr2.length && arr1.every(elem => arr2.includes(elem));
+}
+
+// console.log(isEqual(a3, a2));
+
+//more complete version
+
+function checkEqual(arr1, arr2) {
+    if (arr1.length != arr2.length) {
+        return false;
+    }
+
+    for (let i = 0; i < arr1.length; i++) {
+        if(arr2.includes(arr1[i])) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+console.log(checkEqual(a6, a3));
