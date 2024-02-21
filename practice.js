c7a2d14a2785e23d0ff2297b3db97e37528679c5
KajalPourjalil <rina.skyler@yahoo.com>
2024-02-21 19:36:33 +0300

    Update practice.js

--- practice.js
+++ practice.js
@@ -38,4 +38,48 @@ a5 = [1.54, 2.3, 4.01, 5.9];
 a6 = [-5.04, -4.67, -3.99, -2.89];
 
 
-console.log(mergeSort(a5, a6));
+// console.log(mergeSort(a5, a6));
+
+////////////////////////////////////////////////////////////
+
+function removeDuplicate(arr){
+    for(let i = 0 ; i < arr.length - 1; i++){
+        if(arr[i] === arr[i+1]){
+            arr.splice(i , 1);
+            i--;
+        }
+    }
+    return arr;
+}
+
+let a7 = [5,5,4,4,9,9,10,11,12, 13, 13, 13, 13];
+// console.log(removeDuplicate(a7));
+
+/////////////////////////////////////////////////////////////
+
+function countFreq(arr, n){
+    let count = 0;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] == n){
+            count++;
+        }
+    }
+    return count;
+}
+
+// console.log(countFreq(a7, 13));
+
+/////////////////////////////////////////////////////////////
+//Two sum function
+function twoSum(arr, n){
+    for(let i = 0 ; i < arr.length; i++){
+        let diff = n - arr[i];
+        if(diff > arr[i] && (arr.indexOf(diff) != i)){
+            return [i , arr.indexOf(diff)];
+        }
+    }
+    return false;
+}
+
+let a8 = [2,3,4,5,6,7];
+console.log(twoSum(a8, 7));
